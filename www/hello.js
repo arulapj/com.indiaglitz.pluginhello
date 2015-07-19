cba0f383cfe2dcf4bc1a50fbd2673882f4369652
arul <arul.apj@gmail.com>
2015-07-19 15:15:39 +0530

    updated js

--- www/hello.js
+++ www/hello.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
 
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+    openGallary: function (arrImages, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "openGallary", [arrImages]);
     }
 };
